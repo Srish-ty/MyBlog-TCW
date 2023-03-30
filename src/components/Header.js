780a4031504ac051c69791ea93d6802c7fd187be
Srishty M <srishtymangutte@gmail.com>
2023-03-30 01:56:33 +0000

    Update 4 files

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/header.styles.css";
+
+export const Header = () => {
+  return (
+      <header className='header'>
+          <h1 className='blog-name'>The Code_World</h1>
+      </header>
+  );
+};
